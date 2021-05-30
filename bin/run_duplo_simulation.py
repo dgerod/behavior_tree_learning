7ad1c2052872eada84ffc4fbbc95cb5b2d81d66c
Diego Escudero <dgerod@xyz-lab.org.es>
2021-05-30 11:34:41 +0200

    Remove app scripts as examples should be executed calling main in its directory.

--- bin/run_duplo_simulation.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-
-from behavior_tree_learning.duplo_simulation import main
-
-main.run()
--- bin/run_duplo_state_machine.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-
-from behavior_tree_learning.duplo_state_machine import main
-
-main.run()
