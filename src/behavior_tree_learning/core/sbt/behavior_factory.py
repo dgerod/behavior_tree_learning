a075a1b26616a0b67fe09c6c56f484eac4c9a21a
Diego Escudero <dgerod@xyz-lab.org.es>
2021-06-27 11:19:19 +0200

    Rename argument to make easy to understand the scope of it.

--- src/behavior_tree_learning/core/sbt/behavior_factory.py
+++ src/behavior_tree_learning/core/sbt/behavior_factory.py
@@ -4,36 +4,36 @@ from behavior_tree_learning.core.sbt.behaviors import RSequence
 
 class BehaviorNodeFactory:
     
-    def __init__(self, make_execution_node):
+    def __init__(self, make_execution_node=None):
         self._make_execution_node = make_execution_node
 
-    def make_node(self, text, world, verbose=False):
+    def make_node(self, name, world=None, verbose=False):
 
-        if text == 'nonpytreesbehavior':
+        if name == 'nonpytreesbehavior':
             return None, False
 
         has_children = True
-        node = self._make_control_node(text)
+        node = self._make_control_node(name)
         
-        if node is None:
+        if node is None and self._make_execution_node is not None:
         
             has_children = False
-            node = self._make_execution_node(text, world, verbose)
+            node = self._make_execution_node(name, world, verbose)
         
             if node is None:
-                raise Exception("Unexpected character", text)
+                raise Exception("Unexpected character", name)
 
         return node, has_children
 
-    def _make_control_node(self, text):
+    def _make_control_node(self, name):
         
         node = None
 
-        if text == 'f(':
+        if name == 'f(':
             node = pt.composites.Selector('Fallback')
-        elif text == 's(':
+        elif name == 's(':
             node = RSequence()
-        elif text == 'p(':
+        elif name == 'p(':
             node = pt.composites.Parallel(
                 name="Parallel",
                 policy=pt.common.ParallelPolicy.SuccessOnAll(synchronise=False))
