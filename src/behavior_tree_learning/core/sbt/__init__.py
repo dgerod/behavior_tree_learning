78ed6955786e035fc889549b105eb1dbeec72062
Diego Escudero <dgerod@xyz-lab.org.es>
2021-12-12 06:42:40 +0100

    Add new plot function to sbt module.

--- src/behavior_tree_learning/core/sbt/__init__.py
+++ src/behavior_tree_learning/core/sbt/__init__.py
@@ -4,3 +4,4 @@ from behavior_tree_learning.core.sbt.behavior_factory import BehaviorNodeFactory
 from behavior_tree_learning.core.sbt.execute import BehaviorTreeExecutor
 from behavior_tree_learning.core.sbt.py_tree import StringBehaviorTree, ExecutionParameters
 from behavior_tree_learning.core.sbt.world import World
+from behavior_tree_learning.core.sbt.graphics import plot_behavior_tree
--- src/behavior_tree_learning/sbt.py
+++ src/behavior_tree_learning/sbt.py
@@ -2,8 +2,9 @@ from behavior_tree_learning.core.sbt import BehaviorTreeExecutor, ExecutionParam
 from behavior_tree_learning.core.sbt import BehaviorNodeFactory, BehaviorRegister, \
     BehaviorNode, BehaviorNodeWithOperation
 from behavior_tree_learning.core.sbt import World, StringBehaviorTree
-from behavior_tree_learning.core.sbt import behavior_tree as bt
 from behavior_tree_learning.core.sbt import BehaviorTreeStringRepresentation
+from behavior_tree_learning.core.sbt import plot_behavior_tree
+
+from behavior_tree_learning.core.sbt import behavior_tree as bt
 from behavior_tree_learning.core.sbt.behaviors import RSequence
 from behavior_tree_learning.core.sbt.behavior_tree import load_settings_from_file as load_bt_settings
-
