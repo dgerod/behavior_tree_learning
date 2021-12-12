bd77b5760d8b46dd930f7a83c2ab265333d8e763
Diego Escudero <dgerod@xyz-lab.org.es>
2021-12-12 06:41:54 +0100

    Add function to plot a SBT.

---
+++ src/behavior_tree_learning/core/sbt/graphics.py
@@ -0,0 +1,8 @@
+from behavior_tree_learning.core.sbt.behavior_factory import BehaviorNodeFactory
+from behavior_tree_learning.core.sbt.py_tree import StringBehaviorTree
+
+
+def plot_behavior_tree(bt_name: str, sbt: str, node_factory: BehaviorNodeFactory, directory_path):
+
+    tree = StringBehaviorTree(sbt, behaviors=node_factory, world=None)
+    tree.save_figure(directory_path, name=bt_name)
