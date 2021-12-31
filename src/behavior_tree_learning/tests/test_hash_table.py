f214ff5a2f97711df5c9c7756faae84cb9f0c35a
Diego Escudero <dgerod@xyz-lab.org.es>
2021-12-31 12:08:55 +0100

    Update hash table tests but they do not work yet.

--- src/behavior_tree_learning/tests/test_hash_table.py
+++ src/behavior_tree_learning/tests/test_hash_table.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 
-import unittest
+import paths
+paths.add_modules_to_path()
 
-from behavior_tree_learning.core import hash_table
+import unittest
+from behavior_tree_learning.core.gp import hash_table
 
 
 class TestHastTable(unittest.TestCase):
   
-    def test_hash_table_save_load(self):
-        """ Tests saving and loading of hash table """
+    def test_save_table_and_load(self):
 
         hash_table1 = hash_table.HashTable(size=10)
         hash_table1.insert(['1'], 1)
@@ -16,14 +17,14 @@ class TestHastTable(unittest.TestCase):
         hash_table1.insert(['3'], 3)
         hash_table1.insert(['4'], 4)
         hash_table1.insert(['4'], 5)
-        hash_table1.write_table()
+        hash_table1.write()
+
         hash_table2 = hash_table.HashTable(size=10)
         hash_table2.load()
 
-        assert hash_table1 == hash_table2
+        self.assertEqual(hash_table1, hash_table2)
 
-    def test_hash_table_eq(self):
-        """ Tests hash table comparison function """
+    def test_tables_are_equal(self):
 
         hash_table1 = hash_table.HashTable(size=10)
         hash_table2 = hash_table.HashTable(size=10)
@@ -32,30 +33,28 @@ class TestHastTable(unittest.TestCase):
         hash_table2.insert(['3'], 3)
         hash_table2.insert(['4'], 4)
 
-        assert hash_table1 == hash_table1
-        assert hash_table1 != hash_table2
-        assert hash_table1 != 1
+        self.assertEqual(hash_table1, hash_table1)
+        self.assertNotEqual(hash_table1, hash_table2)
+        self.assertNotEqual(hash_table1, 1)
 
-    def test_node_eq(self):
-        """ Tests node comparison function """
+    def test_nodes_are_equal(self):
 
         node1 = hash_table.Node(['a'], 1)
         node2 = hash_table.Node(['a'], 1)
         node3 = hash_table.Node(['b'], 2)
-        assert node1 == node2
-        assert node1 != node3
-        assert node1 != ['a']
-        assert node1 != 1
+        self.assertEqual(node1, node2)
+        self.assertNotEqual(node1, node3)
+        self.assertNotEqual(node1, ['a'])
+        self.assertNotEqual(node1, 1)
 
         node1.next = node3
-        assert node1 != node2
+        self.assertNotEqual(node1, node2)
 
         node2.next = node3
-        assert node1 == node2
+        self.assertEqual(node1, node2)
 
-    def test_multiple_entries(self):
-        """ Tests multiple entries for the same hash """
-        
+    def test_multiple_entries_in_one_table(self):
+
         hash_table1 = hash_table.HashTable(size=10)
         hash_table1.insert(['a'], 1)
         hash_table1.insert(['a'], 2)
@@ -64,8 +63,8 @@ class TestHastTable(unittest.TestCase):
         hash_table1.insert(['b'], 5)
         hash_table1.insert(['b'], 6)
 
-        assert hash_table1.find(['a']) == [1, 2, 3]
-        assert hash_table1.find(['b']) == [4, 5, 6]
+        self.assertEqual(hash_table1.find(['a']), [1, 2, 3])
+        self.assertEqual(hash_table1.find(['b']), [4, 5, 6])
 
 
 if __name__ == '__main__':
