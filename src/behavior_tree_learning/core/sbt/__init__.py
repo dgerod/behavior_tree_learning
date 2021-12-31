10d0bd1e5483d37006da8a883fc85e3aa0aea4dd
Diego Escudero <dgerod@xyz-lab.org.es>
2021-12-31 11:10:31 +0100

    Rename file.

--- src/behavior_tree_learning/core/sbt/__init__.py
+++ src/behavior_tree_learning/core/sbt/__init__.py
@@ -1,7 +1,7 @@
 from behavior_tree_learning.core.sbt.behavior_tree import BehaviorTreeStringRepresentation
 from behavior_tree_learning.core.sbt.node_factory import BehaviorNodeFactory, BehaviorRegister, \
     BehaviorNode, BehaviorNodeWithOperation
-from behavior_tree_learning.core.sbt.execute import BehaviorTreeExecutor
+from behavior_tree_learning.core.sbt.executor import BehaviorTreeExecutor
 from behavior_tree_learning.core.sbt.py_tree import StringBehaviorTree, ExecutionParameters
 from behavior_tree_learning.core.sbt.world import World
 from behavior_tree_learning.core.sbt.graphics import plot_behavior_tree
