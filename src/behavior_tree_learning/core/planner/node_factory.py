984ffab4559931670e36a731cd465b9bdb576f0f
Diego Escudero <dgerod@xyz-lab.org.es>
2022-01-04 09:39:46 +0100

    Minor modification in planner, it is not working yet.

--- src/behavior_tree_learning/core/planner/node_factory.py
+++ src/behavior_tree_learning/core/planner/node_factory.py
@@ -1,12 +1,12 @@
 import py_trees as pt
-from behavior_tree_learning.core.sbt.behavior_factory import BehaviorNodeFactory as SbtBehaviorFactory
+from behavior_tree_learning.core.sbt.behavior_factory import BehaviorNodeFactory
 
 
-class BehaviorNodeFactory:
+class PlannerBehaviorNodeFactory:
     
     def __init__(self, make_execution_node, get_condition_parameters):
 
-        self._behavior_factory = SbtBehaviorFactory(make_execution_node)
+        self._behavior_factory = BehaviorNodeFactory(make_execution_node)
         self._make_execution_node = make_execution_node
         self._get_condition_parameters = get_condition_parameters
 
@@ -16,7 +16,8 @@ class BehaviorNodeFactory:
 
     def get_node(self, name: str, world, condition_parameters):
         
-        node = SbtBehaviorFactory().make_node(name)
+        node = self._behavior_factory.make_node(name)
+
         if node is None:
             return self._make_execution_node(name, world, condition_parameters)
         else:
--- src/behavior_tree_learning/core/planner/planner.py
+++ src/behavior_tree_learning/core/planner/planner.py
@@ -1,25 +1,25 @@
 """
-Implements a very simple task planner inspired from 'Towards Blended Reactive Planning and Acting using Behavior Trees'
-Generates a behaviors tree to solve task given a set of goals
-and behaviors with preconditions and postconditions. Since the
-conditions are not always static, it actually runs the tree while evaluating
-the conditions.
+Implements a very simple task planner inspired from 'Towards Blended Reactive
+Planning and Acting using Behavior Trees'. Generates a behaviors tree to solve
+task given a set of goals and behaviors with preconditions and postconditions.
+Since the conditions are not always static, it actually runs the tree while
+evaluating the conditions.
 """
 import py_trees as pt
 
 from behavior_tree_learning.core.sbt import StringBehaviorTree, BehaviorNodeFactory
 from behavior_tree_learning.core.sbt.behaviors import RSequence
 from behavior_tree_learning.core.sbt.behavior_tree import get_action_list
-from behavior_tree_learning.core.planner import BehaviorNodeFactory as PlannerBehaviorNodeFactory
+from behavior_tree_learning.core.planner import PlannerBehaviorNodeFactory
 
 
 def _handle_precondition(precondition, behavior_factory, world):
     """
-    Handles precondition by creating a subtree whose
-    postconditions fulfill the precondition
+    Handles precondition by creating a subtree whose post-conditions (aka effects)
+    fulfill the pre-condition (aka condition)
     """
 
-    print("Condition in: ", precondition)
+    print("Pre-condition in: ", precondition)
     condition_parameters = behavior_factory.get_condition_parameters(precondition)
 
     for action in get_action_list():
@@ -27,9 +27,10 @@ def _handle_precondition(precondition, behavior_factory, world):
         action_node, _ = behavior_factory.get_node(action, world, condition_parameters)        
         if precondition in action_node.get_postconditions():
             
-            action_preconditions = action_node.get_preconditions()            
-            if action_preconditions != []:
-                
+            action_preconditions = action_node.get_preconditions()
+
+            if action_preconditions:
+
                 bt = RSequence('Sequence')
                 for action_precondition in action_preconditions:
 
@@ -53,8 +54,9 @@ def _handle_precondition(precondition, behavior_factory, world):
 def _extend_leaf_node(leaf_node, behavior_factory, world):
     """
     If leaf node fails, it should be replaced with a selector that checks leaf node
-    and a subtree that fixes the conditon whenever it's not met.
+    and a subtree that fixes the pre-condition (condition) whenever it's not met.
     """
+
     bt = pt.composites.Selector(name='Fallback')
     leaf_node.parent.replace_child(leaf_node, bt)
     bt.add_child(leaf_node)
@@ -69,13 +71,15 @@ def _expand_tree(node, behavior_factory, world):
     """
     Expands the part of the tree that fails
     """
+
     print("TREE COMING IN :", node)
 
     if node.name == 'Fallback':
         print("Fallback node fails\n")
         for index, child in enumerate(node.children):
-            if index >= 1: #Normally there will only be two children
+            if index >= 1:  # Normally there will only be two children
                 _expand_tree(child, behavior_factory, world)
+
     elif node.name == 'Sequence':
         print("Sequence node fails\n")
         for i in range(len(node.children)):
@@ -83,33 +87,41 @@ def _expand_tree(node, behavior_factory, world):
                 print("Child that fails: ", node.children[i].name)
                 _expand_tree(node.children[i], behavior_factory, world)
     elif isinstance(node, pt.behaviour.Behaviour) and node.status == pt.common.Status.FAILURE:
+
         _extend_leaf_node(node, behavior_factory, world)
+
     else:
         print("Tree", node.name)
 
 
 def plan(world, get_execution_node, get_condition_parameters, goals):
     """
-    Main planner functionbehavior_factoryc, it actually runs the tree while evaluating
-    the conditions.
-    """
+     Generates a behaviors tree to solve task given a set of goals
+     and behaviors with pre-conditions and post-conditions. Since the
+     conditions are not always static, it actually runs the tree while evaluating
+     the conditions.
+     """
     
     tree = RSequence()
     planner_behavior_factory = PlannerBehaviorNodeFactory(get_execution_node, get_condition_parameters)
 
     for goal in goals:
+
         goal_condition, _ = planner_behavior_factory.get_node(goal, world, [])
         tree.add_child(goal_condition)
+
     print(pt.display.unicode_tree(root=tree, show_status=True))
 
     for i in range(60):
+
         tree.tick_once()
         print("Tick: ", i)
         print(pt.display.unicode_tree(root=tree, show_status=True))
+
         if tree.status is pt.common.Status.FAILURE:
             _expand_tree(tree, planner_behavior_factory, world)
-
             print(pt.display.unicode_tree(root=tree, show_status=True))
+
         elif tree.status is pt.common.Status.SUCCESS:
             break
 
